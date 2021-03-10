fd7ead12b255c713e7384a8a4e1996027c9f109e
Conrad Trost <ctrost@tamu.edu>
2021-03-10 12:06:45 -0600

    Added /blog archive and blogs using markdown in src/markdown-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: "Freelance Web Development by Conrad Trost",
-    image: "/src/images/meta-headshot.jpg",
+    title: "Freelance Web Development",
+    titleTemplate: "%s · Conrad Trost",
+    description:
+      "Fast & secure website development by Conrad Trost",
+    url: "https://conradtrost.com.com", // No trailing slash allowed!
+    image: "/images/meta-headshot.jpg", // Path to your image you placed in the 'static' folder
+    twitterUsername: "@occlumency",
   },
   plugins: [
-    `gatsby-plugin-nodejs`,
     "gatsby-plugin-postcss",
     {
       resolve: "gatsby-plugin-manifest",
@@ -20,10 +24,13 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
-        path: "./src/images/",
+        path: "./src/images/",        
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
       },
       __key: "images",
     },
+    "gatsby-transformer-remark",
   {
     resolve: "gatsby-plugin-react-svg",
     options: {
